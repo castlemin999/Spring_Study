4f2b2df7d5cd7c9ad5c00d47feda1a3b31d179ed
castlemin999 <castlemin999@gmail.com>
2022-05-29 23:53:54 +0900

    Hello Spring

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    // http의 GET
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        /*
+        controller에서 리턴값으로 문자를 반환하면 뷰 리졸버가 화면을 찾아서 처리함
+        SpringBoot에서 return은 resources/templates/*.html을 매핑
+        여기서는 return이 hello 이니까 resources/templates/hello.html
+        */
+        return "hello";
+    }
+}
