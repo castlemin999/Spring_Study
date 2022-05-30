d75a25525db0cf7ff2ead5db7da8b368f4c2fe90
Sungmin Choi <castlemin999@gmail.com>
2022-05-30 18:04:26 +0900

    Spring Study

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -16,5 +17,11 @@ public class HelloController {
         여기서는 return이 hello 이니까 resources/templates/hello.html
         */
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
