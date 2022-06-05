78810ee5c192a95f90e61db1359303ae4dd174a9
castlemin999 <castlemin999@gmail.com>
2022-06-05 22:24:32 +0900

    Spring Study

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,22 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
+/*
+* 비즈니스 요구사항 정리
+* 데이터 : 회원ID, 이름
+* 기능 : 회원 등록, 조회
+* 아직 데이터 저장소가 선정되지 않았음.
+* */
 @Controller
 public class MemberController {
 
@@ -12,5 +25,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // createMemberForm.html 화면으로 이동
+    @GetMapping("members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
